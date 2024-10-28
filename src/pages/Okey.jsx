176508b7bda78cc05c5238dc2dd466bb8d33356e
samuelchamizogonzalez@gmail.com <samuelchamizogonzalez@gmail.com>
2024-10-29 00:47:55 +0100

    okey - landing page distribution

--- src/pages/Okey.jsx
+++ src/pages/Okey.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react";
 import './../assets/Styles/Okey.scss'
 import Instagram_icon from "./../assets/Images/Logo_insta_f1f1f1.png"
+import Isotipo from "./../assets/Images/LOGO OKEY 11.png"
 
 function Okey() {
 
@@ -12,11 +13,16 @@ function Okey() {
     <div id="welcome">
       <div className="background"></div>  
       <div className="info">
+        <div className="Isotipo">
+          <img className="isotipo" src={Isotipo} alt="MDN"/>
+        </div>
         <h1 id="name" className="fade_in">okeyeventos</h1>
         <h3>¿Bailamos?</h3>
-        <div className="icons_container">
+      </div>
+      <div className="icons_container">
+        <div className="div_insta">
           <a href="https://www.instagram.com/okeyeventos/" target="_blank" rel="noreferrer"><img src={Instagram_icon} alt="MDN"/></a>  
-        </div>
+        </div> 
       </div>
     </div>
   );
